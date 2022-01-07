778f634dfcc9d1442c360f9fcc09129b39d5d731
Ingrid_Ramos <ingridrcs@gmail.com>
2022-01-07 15:02:11 -0300

    Requisito3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName == null) {
+    const a = {};
+    return a;
+  }
+  const search = employees.find((a) => a.firstName === employeeName || a.lastName === employeeName);
+  console.log(search);
+  return search;
 }
-
+getEmployeeByName();
 module.exports = getEmployeeByName;
