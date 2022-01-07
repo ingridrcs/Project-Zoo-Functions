eb772b34a770b663c378e05e4cfc47e553761509
Ingrid_Ramos <ingridrcs@gmail.com>
2022-01-07 14:28:19 -0300

    Requisitos 1 e 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animais = species.find((item) => item.name === animal);
+  const residentes = animais.residents.every((idade) => idade.age > age);
+  return residentes;
 }
+// getAnimalsOlderThan('penguins',10);
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+const lionId = '0938aa23-f153-4937-9f88-4858b24d6bce';
+const ottersId = '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae';
+// Source: Consegui resolver o requisito através da monitoria com a Carol.
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const names = ids.map((id) => data.species.find((identidade) => identidade.id === id));
+  return names;
+  // const newaa = data.species.filter((objeto)=> ids.includes(objeto.id));
 }
-
+getSpeciesByIds(lionId, ottersId);
 module.exports = getSpeciesByIds;
