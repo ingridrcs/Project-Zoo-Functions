c361e22cb40c8f7aa13b2a72efc677cc0a74f0f4
Ingrid_Ramos <ingridrcs@gmail.com>
2022-01-10 16:28:33 -0300

    Requisitos 5 e 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,32 @@
 const data = require('../data/zoo_data');
-
+// Source: Monitoria do Thales em que explicou como usar o reduce.
+const ent = [{ name: 'ingrid', age: 5 }];
 function countEntrants(entrants) {
   // seu código aqui
+  const countAge = entrants.reduce((acc, { age }) => {
+    if (age < 18) acc.child += 1;
+    if (age >= 18 && age < 50) acc.adult += 1;
+    if (age >= 50) acc.senior += 1;
+    return acc;
+  },
+  { child: 0, adult: 0, senior: 0 });
+  return countAge;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (entrants == null) {
+    return 0;
+  }
+  if (Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const count = countEntrants(entrants);
+  const priceChild = count.child * 20.99;
+  const priceAdult = count.adult * 49.99;
+  const priceSenior = count.senior * 24.99;
+  const total = priceChild + priceAdult + priceSenior;
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,31 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+// Source: https://www.delftstack.com/pt/howto/javascript/array-to-objects-javascript/
+// Aula do course sobre map em que aparece os exemplos de exercícios com map.
+// Monitoria do Thales para dúvidas do projeto em que ele explicou sobre o Object.keys nesssa questão
 
+/* const animalsNames = species.map((item) => item.name);
+    const count = species.map((item) => item.residents.length);
+    const all = (animals, quant) => animals.map((item, index) => `${item}: ${quant[index]} `);
+    const finalArray = all(animalsNames, count).sort();
+    console.log(finalArray);
+    Aplicar o reduce no final , porém não consegui . */
 function countAnimals(animal) {
   // seu código aqui
-}
 
+  if (animal == null) {
+    const order = species.reduce((acc, values) => {
+      acc[values.name] = values.residents.length;
+      return acc;
+    }, {});
+    return order;
+  }
+  if (Object.keys(animal).length === 1) {
+    const count = species.find((item) => item.name === animal.specie).residents.length;
+    return count;
+  }
+  const totalFinal = species.find((item) => item.name === animal.specie).residents
+    .filter((index) => index.sex === animal.sex).length;
+  return totalFinal;
+}
 module.exports = countAnimals;
