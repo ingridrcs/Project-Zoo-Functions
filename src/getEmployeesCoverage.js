595309dd48b22fe56c393f4d41aa2f0ab24b4d2a
Ingrid_Ramos <ingridrcs@gmail.com>
2022-01-14 15:05:01 -0300

    Requisito10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,47 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+
+function checkName(obj) {
+  const identify = employees.find((item) =>
+    item.firstName === obj.name || item.lastName === obj.name || item.id === obj.id);
+  if (identify === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  return identify;
 }
 
+function information(obj) {
+  const newObj = {
+    id: '',
+    fullName: '',
+    species: [],
+    locations: [],
+  };
+  employees.forEach((index) => {
+    if (index.firstName === obj.name || index.lastName === obj.name || index.id === obj.id) {
+      newObj.id = index.id;
+      newObj.fullName = `${index.firstName} ${index.lastName}`;
+      newObj.species = species.filter((item) => index.responsibleFor.includes(item.id))
+        .map((item) => item.name);
+      newObj.locations = species.filter((item) => index.responsibleFor.includes(item.id))
+        .map((item) => item.location);
+    }
+  });
+  return newObj;
+}
+function getEmployeesCoverage(obj) {
+  // seu código aqui
+  if (typeof obj === 'undefined') {
+    const allEmployees = employees.reduce((acc, item) => {
+      acc.push(information(item));
+      return acc;
+    }, []);
+    return allEmployees;
+  }
+  if (checkName(obj)) {
+    const finalObj = information(obj);
+    return finalObj;
+  }
+}
 module.exports = getEmployeesCoverage;
